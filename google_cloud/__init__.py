e11d21cf02ec12ffc0985c5603e2141fa7cb872e
Thw <chanhosatoh@gmail.com>
2022-07-23 14:51:30 +0700

    rm regex P

--- google_cloud/__init__.py
+++ google_cloud/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
-from regex import P
 from .actions import ActionChain
 from .types import PasteType
 from pandas import DataFrame
